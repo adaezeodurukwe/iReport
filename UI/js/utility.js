1377634f4babcd5ac6386833c2c9e0d5806b3824
Adaeze Odurukwe <daizyodurukwe@gmail.com>
2018-11-25 05:27:51 +0100

    Make pages mobile friendly

---
+++ UI/js/utility.js
@@ -0,0 +1,48 @@
+function mobileNav() {
+	var x = document.getElementsByClassName("mobile-nav")[0];
+	if (x.style.display === "block") {
+		x.style.display = "none";
+	} else {
+		x.style.display = "block";
+	}
+}
+
+/**
+* Modal gallery (lightbox) solution from https://www.w3schools.com/howto/howto_js_lightbox.asp
+**/
+function openModal() {
+  	document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  	document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  	showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("demo");
+	var captionText = document.getElementById("caption");
+	if (n > slides.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+	  	slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+	  	dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " active";
+	captionText.innerHTML = dots[slideIndex-1].alt;
+}
