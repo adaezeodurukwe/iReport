44f229285a37e314e47f7b39026449668617cb61
Adaeze Odurukwe <daizyodurukwe@gmail.com>
2018-11-27 20:30:26 +0100

    Create incident endpoint

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -37,13 +37,46 @@ class Incidents {
         const id = parseInt(req.params.id, 10);
         try {
             const record = await Model.getOne(id);
+            if (record === false) {
+                return res.status(404).send({
+                    status: 404,
+                    data: 'Not found',
+                });
+            }
             return res.status(200).send({
                 status: 200,
                 data: record,
             });
         } catch (error) {
             return res.status(404).send({
-                status: 404,
+                message: error,
+            });
+        }
+    }
+
+    /**
+     * @class createIncident
+     * @param {*} req
+     * @param {*} res
+     * @returns newRecord
+     */
+    static async createIncident(req, res) {
+        try {
+            const newRecord = await Model.create(
+                req.body.author,
+                req.body.type,
+                req.body.location,
+                req.body.comment,
+            );
+            return res.status(201).send({
+                status: 201,
+                data: [{
+                    id: newRecord,
+                    message: 'Created red-flag record',
+                }],
+            });
+        } catch (error) {
+            return res.status(400).send({
                 message: error,
             });
         }
--- server/model/model.js
+++ server/model/model.js
@@ -1,5 +1,6 @@
 // Model class
 
+import moment from 'moment';
 import incidents from '../db/database';
 
 /**
@@ -16,15 +17,43 @@ class Model {
 
     /**
      * @static getOne
+     * @param {*} id
      * @returns {object}
      */
     static getOne(id) {
         const incidentId = id;
-        let record;
+        let record = false;
         incidents.forEach((incident) => {
-            record = incident.id === incidentId ? incident : false;
+            if (incident.id === incidentId) {
+                record = incident;
+            }
+            return record;
         });
         return record;
+    }
+
+    /**
+     * @async create
+     * @param {*} author
+     * @param {*} incidentType
+     * @param {*} incidentLocation
+     * @param {*} comments
+     * @returns {object} incident
+     */
+    static create(author, incidentType, incidentLocation, comments) {
+        const incidentId = incidents.length + 1;
+        const newIncident = {
+            id: incidentId,
+            createdBy: author,
+            createdOn: moment().format('DD/MM/YYYY'),
+            type: incidentType,
+            location: incidentLocation,
+            status: 'draft',
+            images: [],
+            videos: [],
+            comment: comments,
+        };
+        return incidents.push(newIncident);
     }
 }
 
