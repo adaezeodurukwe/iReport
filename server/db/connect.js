9359cf166f093e3e5d6ecb1b4c6ce52eefde9e67
Adaeze Odurukwe <daizyodurukwe@gmail.com>
2018-12-06 12:34:05 +0100

    Set up database
    - Download pg
    - Set up connection to database
    - Create table schema
    
    [Delivers #162468337]

---
+++ server/db/connect.js
@@ -0,0 +1,12 @@
+// Connect to database
+
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+export default pool;
