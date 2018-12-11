1f9291f07054631a4068e806b9255dee1a68864b
Adaeze Odurukwe <daizyodurukwe@gmail.com>
2018-12-11 22:19:18 +0100

    Create middleware functions for validation
    - Create validation middleware
    - Add validation functions to routes
    
    [Delivers #162572235]

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -35,7 +35,7 @@ class User {
                 }],
             });
         } catch (error) {
-            return res.status(404).send({
+            return res.status(500).send({
                 message: error,
             });
         }
@@ -64,7 +64,7 @@ class User {
                 }],
             });
         } catch (error) {
-            return res.status(404).send({
+            return res.status(500).send({
                 message: error,
             });
         }
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,22 +1,72 @@
 // Routes
 
 import express from 'express';
-// import Incidents from '../controller/controller';
 import User from '../controller/userController';
 import Records from '../controller/redflagController';
 import Auth from '../middleware/auth';
+import * as Validate from '../middleware/validateInput';
 
 const router = express.Router();
 
-router.post('/red-flags/', Auth, Records.createRecord);
-router.get('/red-flags/', Auth, Records.getAllRedflags);
-router.get('/red-flags/:id', Auth, Records.getOneRedflag);
-router.patch('/red-flags/:id/location', Auth, Records.updateRedflagLocation);
-router.patch('/red-flags/:id/comment', Auth, Records.updateRedflagComment);
-router.delete('/red-flags/:id', Auth, Records.deleteRedflag);
+
+// Red flag routes
+router.post(
+    '/red-flags/',
+    Validate.record,
+    Validate.validationHandler,
+    Auth,
+    Records.createRecord,
+);
+
+router.get(
+    '/red-flags/',
+    Auth,
+    Records.getAllRedflags,
+);
+
+router.get(
+    '/red-flags/:id',
+    Auth,
+    Validate.parameter,
+    Records.getOneRedflag,
+);
+
+router.patch(
+    '/red-flags/:id/location',
+    Validate.updateLocation,
+    Validate.validationHandler,
+    Auth,
+    Records.updateRedflagLocation,
+);
+
+router.patch(
+    '/red-flags/:id/comment',
+    Validate.updateComment,
+    Validate.validationHandler,
+    Auth,
+    Records.updateRedflagComment,
+);
+
+router.delete(
+    '/red-flags/:id',
+    Auth,
+    Records.deleteRedflag,
+);
+
 
 // User routes
-router.post('/auth/signup', User.createUser);
-router.post('/auth/signin', User.login);
+router.post(
+    '/auth/signup',
+    Validate.signUp,
+    Validate.validationHandler,
+    User.createUser,
+);
+
+router.post(
+    '/auth/signin',
+    Validate.signin,
+    Validate.validationHandler,
+    User.login,
+);
 
 export default router;
