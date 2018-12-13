78438154945c3a7ec513f9cf4ffbe98e5a6d836a
Adaeze Odurukwe <daizyodurukwe@gmail.com>
2018-12-13 10:14:48 +0100

    fix tests

--- test/recordstest.js
+++ test/recordstest.js
@@ -470,7 +470,7 @@ describe('before testing', () => {
         describe('PATCH API endpoint /red-flags/<red-flag-id>/status', () => {
             it('should update status on a red flag', (done) => {
                 chai.request(app)
-                    .patch(`/api/v1/red-flag/${redflagId}/status/`)
+                    .patch(`/api/v1/red-flags/${redflagId}/status/`)
                     .set('x-access-token', admintoken)
                     .send({
                         status: 'resolved',
@@ -556,7 +556,7 @@ describe('before testing', () => {
 
             it('should return forbidden', (done) => {
                 chai.request(app)
-                    .patch(`/api/v1/red-flag/${interventionId}/status/`)
+                    .patch(`/api/v1/interventions/${interventionId}/status/`)
                     .set('x-access-token', token)
                     .send({
                         status: 'resolved',
