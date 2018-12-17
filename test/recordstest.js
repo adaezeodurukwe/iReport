9967abd620fa6044be70b47e605f3d5a56e93024
Adaeze Odurukwe <daizyodurukwe@gmail.com>
2018-12-17 18:30:29 +0100

    fix tests

--- test/recordstest.js
+++ test/recordstest.js
@@ -359,7 +359,7 @@ describe('before testing', () => {
                     .patch(`/api/v1/red-flags/${redflagId}/location`)
                     .set('x-access-token', token)
                     .send({
-                        location: 'Lat: 500, Long: 70',
+                        location: '5.00, 7.02',
                     })
                     .end((err, res) => {
                         expect(res).to.have.status(200);
@@ -378,7 +378,7 @@ describe('before testing', () => {
                     .patch('/api/v1/red-flags/3818ea1f-bb6c-43bf-9503-d48957c8a6d3/location')
                     .set('x-access-token', token)
                     .send({
-                        location: 'Lat: 500, Long: 70',
+                        location: '5.00, 7022',
                     })
                     .end((err, res) => {
                         expect(res).to.have.status(404);
@@ -395,7 +395,7 @@ describe('before testing', () => {
                     .patch(`/api/v1/interventions/${interventionId}/location`)
                     .set('x-access-token', token)
                     .send({
-                        location: 'Lat: 500, Long: 70',
+                        location: '5.00, 70.000',
                     })
                     .end((err, res) => {
                         expect(res).to.have.status(200);
@@ -505,7 +505,8 @@ describe('before testing', () => {
                     .end((err, res) => {
                         expect(res).to.have.status(422);
                         expect(res.body).to.be.an('object');
-                        expect(res.body).to.have.property('errors').to.be.an('array');
+                        expect(res.body).to.have.property('status');
+                        expect(res.body).to.have.property('message');
                         done();
                     });
             });
@@ -557,7 +558,8 @@ describe('before testing', () => {
                     .end((err, res) => {
                         expect(res).to.have.status(422);
                         expect(res.body).to.be.an('object');
-                        expect(res.body).to.have.property('errors').to.be.an('array');
+                        expect(res.body).to.have.property('status');
+                        expect(res.body).to.have.property('message');
                         done();
                     });
             });
