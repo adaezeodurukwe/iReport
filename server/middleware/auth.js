82b2a5bf5c83221b1f28f04c7b4e01e6d0a16a74
Adaeze Odurukwe <daizyodurukwe@gmail.com>
2018-12-20 05:32:08 +0100

    Handle expired token error

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,15 +4,25 @@ import * as Helper from '../controller/helper';
 
 const Auth = async (req, res, next) => {
     const token = req.headers['x-access-token'];
-    if (!token) {
-        return res.status(401).send({ message: 'unauthorized' });
+    try {
+        if (!token) {
+            return res.status(401).send({
+                status: 401,
+                message: 'unauthorized',
+            });
+        }
+        const unsigned = await Helper.verifyToken(token);
+
+        req.userId = unsigned.userId;
+        req.isAdmin = unsigned.isAdmin;
+
+        return next();
+    } catch (error) {
+        return res.status(401).send({
+            status: 401,
+            message: error.message,
+        });
     }
-
-    const unsigned = await Helper.verifyToken(token);
-
-    req.userId = unsigned.userId;
-    req.isAdmin = unsigned.isAdmin;
-    return next();
 };
 
 export default Auth;
